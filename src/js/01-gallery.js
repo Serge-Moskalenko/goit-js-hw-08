0fc142f0d3adf112899cd66d8d0b4697f6fdaf95
Serge-Moskalenko <skmos2019@outlook.com>
2022-09-20 15:43:38 +0100

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const refs = {
+    divEl:document.querySelector('.gallery')
+}
+
+function createMarkup(e) {
+    return e.map(ev => {
+
+        return `
+  <a class="gallery__item" href="${ev.original}">
+    <img
+      class="gallery__image"
+      src="${ev.preview}"
+      data-source="${ev.original}"
+      alt='${ev.description}'
+    />
+  </a>
+</div>`
+    }).join('');
+};
+
+const markup = createMarkup(galleryItems);
+
+refs.divEl.insertAdjacentHTML('beforeend', markup);
+
+var lightbox = new SimpleLightbox('.gallery .gallery__item', {captionsData:'alt',captionDelay:250});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,21 @@
+import throttle from "lodash.throttle";
+
+const formEl = document.querySelector('.feedback-form');
+
+const formData = {};
+
+formEl.addEventListener('input', onInput);
+
+formEl.addEventListener('submit', onSubmit);
+
+function onInput(e) {
+    formData[e.target.name] = e.target.value;
+
+    console.log(formData)
+};
+
+function onSubmit(e) {
+    e.preventDefault();
+    e.currentTarget.reset()
+}
 
