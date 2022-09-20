4c639305118824f93609fde7714df645a90c6aac
Serge-Moskalenko <skmos2019@outlook.com>
2022-09-20 16:49:59 +0100

    test

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,8 +8,11 @@ formEl.addEventListener('input', onInput);
 
 formEl.addEventListener('submit', onSubmit);
 
+filledForm()
+
 function onInput(e) {
     formData[e.target.name] = e.target.value;
+    localStorage.setItem("feedback-form-state",JSON.stringify(formData))
 
     console.log(formData)
 };
@@ -17,5 +20,14 @@ function onInput(e) {
 function onSubmit(e) {
     e.preventDefault();
     e.currentTarget.reset()
+};
+
+function filledForm() {
+    const messege = localStorage.getItem(JSON.parse("feedback-form-state"));
+    if (messege) {
+        formEl.value = messege;
+
+        console.log(formEl.value)
+    }
 }
 
